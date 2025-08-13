e05789423e67f424b2fc3ee3e57a6832792fae09
Vittorio Palmisano <vpalmisano@gmail.com>
2025-08-13 13:23:23 +0200

    refactor: remove close button from graph and update canvas styling

--- src/graph.ts
+++ src/graph.ts
@@ -20,18 +20,10 @@ export class Graph {
         this.div.style.cssText = `position:fixed;top:0;right:0;width:${this.width * 2}px;height:${this.height}px;z-index:99999;background-color:black;`;
         document.body.appendChild(this.div);
 
-        const closeBtn = document.createElement('button');
-        closeBtn.textContent = 'X';
-        closeBtn.style.cssText = `margin:0;padding:2px;color:white;background-color:transparent;width:10px;height:10px;position:absolute;top:0px;right:1px;font-family:Sans;font-size:10px;line-height:10px;border:0px;`;
-        closeBtn.addEventListener('click', () => {
-            this.remove();
-        });
-        this.div.appendChild(closeBtn);
-
         this.canvas = document.createElement('canvas');
         this.canvas.width = this.width * 4;
         this.canvas.height = this.height * 2;
-        this.canvas.style.cssText = `width:100%;height:100%;margin-top:10px;`;
+        this.canvas.style.cssText = `width:100%;height:100%;`;
         this.div.appendChild(this.canvas);
 
         this.ctx = this.canvas.getContext('2d');
@@ -53,7 +45,7 @@ export class Graph {
         const { width, height } = this.canvas;
         ctx.fillStyle = 'black';
         ctx.fillRect(0, 0, width, height);
-        ctx.fillStyle = 'red';
+        ctx.fillStyle = 'darkred';
         const w = width / this.data.length;
         const max = Math.max(...this.data);
         this.data.forEach((value, i) => {
@@ -62,7 +54,7 @@ export class Graph {
         });
         if (this.data.length) {
             ctx.fillStyle = 'white';
-            ctx.fillText(this.data[this.data.length - 1].toPrecision(2), 2, 20);
+            ctx.fillText(this.data[this.data.length - 1].toPrecision(2) + ' fps', 2, 20);
         }
     }
 
